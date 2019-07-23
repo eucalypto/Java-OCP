153fb13c4fb7c7e984877872e1b7a6259334d515
Viktor Papara <eucalyptov@gmail.com>
2019-07-23 13:06:33 +0200

    Add first work on Exam taking task

---
+++ src/_07_23/Exam.java
@@ -0,0 +1,51 @@
+// This program helps preparing an exam, executing it with subjects and
+// presents the scores
+
+package _07_23;
+
+import java.util.*;
+
+public class Exam {
+    List questions = new ArrayList<Question>();
+    List examTakers = new ArrayList<ExamTaker>();
+
+    void setUpQuestionnaire() {
+
+    }
+
+    void executeExam(){
+
+    }
+
+    void evaluation(){
+
+    }
+
+    public static void main(String[] args) {
+        Exam exam = new Exam();
+        exam.setUpQuestionnaire();
+        exam.executeExam();
+        exam.evaluation();
+
+    }
+}
+
+
+
+
+
+abstract class Question {
+    int points;
+}
+
+class MultipleChoiceQuestion extends Question {
+}
+
+
+
+
+
+
+class ExamTaker {
+    int points;
+}
