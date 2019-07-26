af7631b31ee1865aa1cd33fc71e2b4fcb6aeded1
Viktor Papara <eucalyptov@gmail.com>
2019-07-26 15:01:41 +0200

    Add playing-around files

---
+++ src/_07_26/CollectionsVsArrays.java
@@ -0,0 +1,13 @@
+package _07_26;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionsVsArrays {
+    public static void main(String[] args) {
+        List<int> tList = new List<int>({1, 3, 4, 5, 6, 5});
+        Collections.sort(tList);
+        Arrays.sort();
+    }
+}
