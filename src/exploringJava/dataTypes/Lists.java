51aa417719b0d2f36cd5f7576ceb2da9820a8e8a
Viktor Papara <eucalyptov@gmail.com>
2019-08-09 12:38:34 +0200

    Add exploration of lists

---
+++ src/exploringJava/dataTypes/Lists.java
@@ -0,0 +1,14 @@
+package exploringJava.dataTypes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lists {
+    public static void main(String[] args) {
+        int[] intlist = new int[10];
+        System.out.println(intlist.length);
+        List<Integer> integerlist = new ArrayList<Integer>();
+        integerlist.remove(1);
+
+    }
+}
