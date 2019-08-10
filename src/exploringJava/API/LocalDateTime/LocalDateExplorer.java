56940feddbc90da5fbe9b5f2d2f2ca2e12f95e38
Viktor Papara <eucalyptov@gmail.com>
2019-08-10 22:39:09 +0200

    Update LocalDateExplorer

--- src/exploringJava/API/LocalDateTime/LocalDateExplorer.java
+++ src/exploringJava/API/LocalDateTime/LocalDateExplorer.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 public class LocalDateExplorer {
     public static void main(String[] args) {
         LocalDate now = LocalDate.now();
+        now.getMonth().getValue();
     }
 }
 
