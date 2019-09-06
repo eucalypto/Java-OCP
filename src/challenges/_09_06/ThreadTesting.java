0016da3127abb4530fc7572458520dd34303b724
Viktor Papara <eucalyptov@gmail.com>
2019-09-06 19:20:57 +0200

    Add scratch file for today

---
+++ src/challenges/_09_06/ThreadTesting.java
@@ -0,0 +1,10 @@
+package challenges._09_06;
+
+import static java.lang.Thread.yield;
+
+public class ThreadTesting {
+    public static void main(String[] args) {
+        yield();
+        // wait()
+    }
+}
