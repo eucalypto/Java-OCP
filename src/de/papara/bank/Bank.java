f0f9855014a882404c4fc62b2befc48774e81833
Viktor Papara <eucalyptov@gmail.com>
2019-09-08 22:42:46 +0200

    Extend class Bank for bank

--- src/de/papara/bank/Bank.java
+++ src/de/papara/bank/Bank.java
@@ -1,7 +1,45 @@
 package de.papara.bank;
 
+
+import de.papara.bank.client.Client;
+
+import java.util.Set;
+
 public class Bank {
     private String name;
     private String bic;
-    private Address addr;
+    private Address address;
+    private Set<Client> clients;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBic() {
+        return bic;
+    }
+
+    public void setBic(String bic) {
+        this.bic = bic;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Set<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(Set<Client> clients) {
+        this.clients = clients;
+    }
 }
