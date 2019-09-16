e4dd894e7a1479560aff7b5fe26dba4eaaddda2f
Viktor Papara <eucalyptov@gmail.com>
2019-09-16 20:10:32 +0200

    Make file compile (by commenting-out)

--- src/challenges/_09_16/Misc.java
+++ src/challenges/_09_16/Misc.java
@@ -6,7 +6,7 @@ public class Misc {
     AtomicInteger aint;
 
     public static void main(String[] args) {
-        aint.set(); // not atomic
-        aint.compareAndSet(); // atomic
+        // aint.set(); // not atomic
+        // aint.compareAndSet(); // atomic
     }
 }
