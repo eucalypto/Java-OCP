28c4cafa9679da8dcc6f68139cc44932fd5c6de8
Viktor Papara <eucalyptov@gmail.com>
2019-09-26 20:01:20 +0200

    Add files from last few days

---
+++ src/challenges/_09_23/ConcurrentCollections.java
@@ -0,0 +1,11 @@
+package challenges._09_23;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class ConcurrentCollections {
+    public static void main(String[] args) {
+        List<Integer> myList = new CopyOnWriteArrayList<>();
+
+    }
+}
---
+++ src/challenges/_09_24/Misc.java
@@ -0,0 +1,25 @@
+package challenges._09_24;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
+public class Misc {
+    public static void main(String[] args) {
+
+    }
+}
+
+class MyDelay implements Delayed {
+
+    @Override
+    public long getDelay(@NotNull TimeUnit unit) {
+        return 0;
+    }
+
+    @Override
+    public int compareTo(@NotNull Delayed o) {
+        return 0;
+    }
+}
