6ab6369ac4ebf4c2709703f233b88a4c34a2851c
Viktor Papara <eucalyptov@gmail.com>
2019-09-27 10:06:53 +0200

    Outsource code for parsing a csv file into List<Person>

--- src/daily/_09_26/PersonHandler.java
+++ src/daily/_09_26/PersonHandler.java
@@ -1,28 +1,10 @@
 package daily._09_26;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 public class PersonHandler {
     public static void main(String[] args) {
-        // Read persons from file
-        Path csvFile = Paths.get("src/daily/_09_26/people.csv");
-        System.out.println(Files.exists(csvFile));
-
-        List<Person> people = new ArrayList<>();
-
-        try {
-            List<String> people_raw = Files.readAllLines(csvFile);
-            // System.out.println("people_raw = " + people_raw);
-            people_raw.forEach(s -> people.add(Persons.ofString(s)));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        List<Person> people = Persons.parseFile("src/daily/_09_26/people.csv");
         System.out.println("people = " + people);
     }
 }
--- src/daily/_09_26/Persons.java
+++ src/daily/_09_26/Persons.java
@@ -1,6 +1,11 @@
 package daily._09_26;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,5 +26,21 @@ public abstract class Persons {
         // not have been initialized
 
         return new Person(entriesOfLine.get(0), LocalDate.parse(entriesOfLine.get(1)), sex, entriesOfLine.get(3));
+    }
+
+    public static List<Person> parseFile(String path) {
+        // Read persons from file
+        Path csvFile = Paths.get(path);
+        List<Person> people = new ArrayList<>();
+        try {
+            List<String> people_raw = Files.readAllLines(csvFile);
+            // people_raw.forEach(s -> people.add(Persons.ofString(s)));
+            // IDEA suggest instead of above line to do:
+            // people = people_raw.stream().map(Persons::ofString).collect(Collectors.toList());
+            people_raw.stream().map(Persons::ofString).forEach(people::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return people;
     }
 }
